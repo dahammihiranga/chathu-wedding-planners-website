9185b814ad6845a26532c51b30b890c06dd776f2
Daham <daham@creatit.com.au>
2026-08-07 10:06:26 +0530

    next wedding countdown added

--- src/data/countdown.ts
+++ src/data/countdown.ts
@@ -1,10 +1,10 @@
 export const featuredWedding = {
-  coupleNames: "Anuradha & Gayan",
+  coupleNames: "Kaushi & Binal",
 
   // Format: YYYY-MM-DDTHH:mm:ss+05:30
-  date: "2026-08-06T09:00:00+05:30",
+  date: "2026-08-14T09:00:00+05:30",
 
-  displayDate: "06 August 2026",
-  venue: "Pabavi Regency - Balummahara",
+  displayDate: "14 August 2026",
+  venue: "Taj Samudra Hotel",
   label: "Our Next Beautiful Celebration",
 };
