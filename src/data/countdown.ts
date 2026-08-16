fa9a70d57d69e5bb008b7bf4bf5f9af030cd68b7
Daham <daham@creatit.com.au>
2026-08-16 10:23:22 +0530

    next wedding countdown date change

--- src/data/countdown.ts
+++ src/data/countdown.ts
@@ -2,7 +2,7 @@ export const featuredWedding = {
   coupleNames: "Hasanthika & Chanaka",
 
   // Format: YYYY-MM-DDTHH:mm:ss+05:30
-  date: "2026-08-14T09:00:00+05:30",
+  date: "2026-08-19T09:00:00+05:30",
 
   displayDate: "19 August 2026",
   venue: "Paradise Inn Bolgoda",
