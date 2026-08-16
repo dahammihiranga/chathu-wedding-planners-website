a49727a6492308d582947a5119fcf153be102398
Daham <daham@creatit.com.au>
2026-08-16 10:16:21 +0530

    next wedding countdown

--- src/data/countdown.ts
+++ src/data/countdown.ts
@@ -1,10 +1,10 @@
 export const featuredWedding = {
-  coupleNames: "Kaushi & Binal",
+  coupleNames: "Hasanthika & Chanaka",
 
   // Format: YYYY-MM-DDTHH:mm:ss+05:30
   date: "2026-08-14T09:00:00+05:30",
 
-  displayDate: "14 August 2026",
-  venue: "Taj Samudra Hotel",
+  displayDate: "19 August 2026",
+  venue: "Paradise Inn Bolgoda",
   label: "Our Next Beautiful Celebration",
 };
