dd77c07027d7b4212f90e59f13605e84031bbe05
Daham <daham@creatit.com.au>
2026-09-02 09:09:50 +0530

    next wedding

--- src/data/countdown.ts
+++ src/data/countdown.ts
@@ -1,10 +1,10 @@
 export const featuredWedding = {
-  coupleNames: "Amali & Imalka",
+  coupleNames: "Shanika & Rayan",
 
   // Format: YYYY-MM-DDTHH:mm:ss+05:30
-  date: "2026-08-27T09:00:00+05:30",
+  date: "2026-09-07T09:00:00+05:30",
 
-  displayDate: "27 August 2026",
+  displayDate: "07 September 2026",
   venue: "Paradise Inn Bolgoda",
   label: "Our Next Beautiful Celebration",
 };
